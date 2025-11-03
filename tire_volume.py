6f8a1ba479ad31068f03a28409e42ebcc290daa6
David-owolabi <davidowolabi252@gmail.com>
2025-11-03 20:37:41 +0100

    Refactor tire volume logging: change log file to volume.txt and improve formatting

--- tire_volume.py
+++ tire_volume.py
@@ -2,15 +2,15 @@ from datetime import datetime
 import math
 tire_width = int(input("Enter the width of the tire in mm (ex 205): "))
 aspect_ratio = float(input("Enter the aspect ratio of the tire (ex 60): "))
-wheel_diameter = int(input("Enter the diameter of the wheel in inches (ex 15) "))
+wheel_diameter = int(input("Enter the diameter of the wheel in inches (ex 15): "))
 tire_volume = math.pi * tire_width**2 * aspect_ratio*(tire_width * aspect_ratio + 2540 * wheel_diameter)/10000000000
 
 print(f"The approximate volume is {tire_volume:.2f} liters")
 current_date_time = datetime.now()
 formatted_date = current_date_time.strftime("%Y-%m-%d")
 
-with open("log_data.txt", "a") as file:
-  file.write(f"\nDate: {formatted_date}\nTire Width: {tire_width}\nAspect ratio: {aspect_ratio}\nWheel diameter: {wheel_diameter}\nTire volume: {tire_volume:.2f}\n")
+with open("volume.txt", "a") as file:
+  file.write(f"\nDate: {formatted_date}\nTire Width: {tire_width}\nAspect ratio: {aspect_ratio:.2f}\nWheel diameter: {wheel_diameter}\nTire volume: {tire_volume:.2f}\n")
   print("Data successfully added to log_data.txt")
 
   if tire_width == 225 and aspect_ratio == 135 and wheel_diameter == 15:
@@ -32,7 +32,9 @@ with open("log_data.txt", "a") as file:
 buy_tires = input("Do you want to buy tires with these specifications? (yes/no): ").strip().lower()
 if buy_tires == "yes" and price != None:
   contact_information = input("Please provide your phone number: ")
-  with open("log_data.txt", "a") as file:
+  with open("volume.txt", "a") as file:
     file.write(f"Customer Phone Number: {contact_information}\n")
     print("Thank you for your purchase! We will contact you soon.")
+else:
+  print("Thank you for using the tire volume calculator.")
   
